497734ba956bc6f1a20f589b3080ed6ad258427c
tatsuya fukui <nakedtatsuya6@gmail.com>
2019-03-03 21:18:03 +0900

    add config

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("Hello! Node.js × TypeScript");
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+app.get('/', (req, res) => {
+    console.log(process.env);
+    res.send('API ss');
+});
+server.listen(3001, () => {
+    console.log(`server start port ${3001}`);
+    console.log(process.env);
+});
 //# sourceMappingURL=app.js.map
---
+++ config/webpack.config.dev.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+		watch: true,
+		mode: 'development',
+		entry: './app.ts',     // src下に書いていくので　src/server.tsにしとく
+		target: 'node',               // Module not found: Error: Can't resolve 'fs'とかいっぱい出たら、この行書き忘れ
+		externals: [nodeExternals()],
+		devtool: 'inline-source-map',
+		module: {
+				rules: [
+						{
+								enforce: 'pre',
+								loader: 'tslint-loader',
+								test: /\.ts$/,
+								exclude: [
+										/node_modules/
+								],
+								options: {
+										emitErrors: true
+								}
+						},
+						{
+								loader: 'ts-loader',
+								test: /\.ts$/,
+								exclude: [
+										/node_modules/
+								],
+								options: {
+										configFile: 'tsconfig.dev.json'
+								}
+						}
+				]
+		},
+		resolve: {
+				extensions: [ '.ts', '.js' ]
+		},
+		output: {
+				filename: 'app.js',
+				path: path.resolve(__dirname, 'dist')
+		}
+};
